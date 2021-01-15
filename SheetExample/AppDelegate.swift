a95ee6994eac68602fe9cc6efbda0a83883557c3
Kevin Gutowski <kskiviolin@gmail.com>
2021-01-14 20:57:10 -0800

    Initial Commit

---
+++ SheetExample/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  SheetExample
+//
+//  Created by Kevin Gutowski on 1/14/21.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
